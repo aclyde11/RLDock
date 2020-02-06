aa456dee13e49c7e158f89c1350fccaeff2d7cda
Austin Clyde <aclyde@uchicago.edu>
2020-02-06 09:51:40 -0600

    fixed_yaml

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ config = {
     'discrete' : True,
     'K_trans' : 4,
     'K_theta' : 4,
-    'normalize' : False,
+    'normalize' : True,
     'action_space_d' : (2, 2, 2),
     'action_space_r' : (2, 2, 2, 2, 2, 2),
     'protein_wo_ligand' :  path + '/test3.pdb',
