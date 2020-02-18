f1f962f7e9fd0e49e59cedfd86f95add36bf98d8
Austin Clyde <aclyde@uchicago.edu>
2020-02-18 14:22:39 -0600

    fixed_yaml

--- rnntest.py
+++ rnntest.py
@@ -263,7 +263,7 @@ if __name__ == "__main__":
         # execute per train batch).
         "num_sgd_iter": 15,
         # Stepsize of SGD.
-        "lr": 1e-5,
+        "lr": 8e-5,
         # Learning rate schedule.
         "lr_schedule": None,
         # Share layers for value function. If you set this to True, it's important
@@ -273,7 +273,7 @@ if __name__ == "__main__":
         # you set vf_share_layers: True.
         "vf_loss_coeff": 0.5,
         # Coefficient of the entropy regularizer.
-        "entropy_coeff": 0.001,
+        "entropy_coeff": 0.0001,
         # Decay schedule for the entropy regularizer.
         "entropy_coeff_schedule": None,
         # PPO clip parameter.
